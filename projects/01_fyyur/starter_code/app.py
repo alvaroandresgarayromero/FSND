363d0cd6f078b6de95b8a53aef46e56e96c7400b
alvaroandresgarayromero <garayromero510@gmail.com>
2021-03-18 23:05:15 -0400

    porting models to their own file

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -14,6 +14,7 @@ from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
 from flask_migrate import Migrate
+from Models import *
 
 # import sys
 # sys.path.append("/home/alvaro/PycharmProjects/FullStack_Project1/FSND/projects/01_fyyur/starter_code")
@@ -28,60 +29,9 @@ app.config.from_object('config')
 
 print(app.config['SQLALCHEMY_DATABASE_URI'])
 
-db = SQLAlchemy(app)
+db.init_app(app)
+
 migrate = Migrate(app, db, compare_type=True)
-
-
-# ----------------------------------------------------------------------------#
-# Models.
-# ----------------------------------------------------------------------------#
-
-# parent
-class Venue(db.Model):
-    __tablename__ = 'Venue'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.PickleType)
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    website = db.Column(db.String(120))
-    seeking_talent = db.Column(db.Boolean)
-    seeking_description = db.Column(db.String(120))
-
-    shows = db.relationship('Show_Table', backref='venues', lazy='select')
-
-
-# parent
-class Artist(db.Model):
-    __tablename__ = 'Artist'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.PickleType)
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    website = db.Column(db.String(120))
-    seeking_venue = db.Column(db.Boolean)
-    seeking_description = db.Column(db.String(120))
-    shows = db.relationship('Show_Table', backref='artists', lazy='select')
-
-
-# child
-class Show_Table(db.Model):
-    __tablename__ = 'Show_Table'
-
-    id = db.Column(db.Integer, primary_key=True)
-    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
-    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
-    start_time = db.Column(db.DateTime)
 
 
 # ----------------------------------------------------------------------------#
